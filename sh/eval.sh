c90b11f9538814464a781ba62f34d3bbb50bb355
qiyu-idea1 <qiyu6@illinois.edu>
2025-09-08 21:03:09 -0500

    upload res

--- sh/eval.sh
+++ sh/eval.sh
@@ -1,4 +1,8 @@
-python utils/evaluate/EM_dir_test.py res/PIQA/dev/Mistral+OpenChat/vanilla
-python utils/evaluate/EM_dir_test.py res/PIQA/dev/Mistral+OpenChat/tas
-python utils/evaluate/EM_dir_test.py res/PIQA/dev/Mistral+OpenChat/tas2
-python utils/evaluate/EM_dir_test.py res/PIQA/dev/Mistral+OpenChat/tas2+mas2
+task=NQ
+run_mode=test
+models=OpenChat+InternLM7b
+
+python utils/evaluate/EM_dir_test.py res/${task}/${run_mode}/${models}/vanilla
+python utils/evaluate/EM_dir_test.py res/${task}/${run_mode}/${models}/tas
+# python utils/evaluate/EM_dir_test.py res/${task}/${run_mode}/${models}/tas2
+python utils/evaluate/EM_dir_test.py res/${task}/${run_mode}/${models}/tas2+mas2
