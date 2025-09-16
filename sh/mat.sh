96cb8e2323d5319511762fa83103e9abbc04573b
qiyu <qiyu6@illinois.edu>
2025-09-15 21:55:09 -0500

    update

---
+++ sh/mat.sh
@@ -0,0 +1,6 @@
+export CUDA_VISIBLE_DEVICES=0,1,2,3
+
+model1=internlm/internlm2_5-7b-chat
+model2=Qwen/Qwen3-4B-Instruct-2507
+
+python src/transfer_matrix/cal_and_save_transfer_matrix.py mat ${model1} ${model2}
